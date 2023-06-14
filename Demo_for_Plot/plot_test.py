b5fed7d08a89aa40c7bc287148316aaeb0bd3d30
shootergod <shootergod@foxmail.com>
2023-06-14 22:16:50 +0800

    1st commit

--- Demo_for_Plot/plot_test.py
+++ Demo_for_Plot/plot_test.py
@@ -1,3 +1,17 @@
+# ============================================================
+# Doc Info
+# ============================================================
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    : plot_test.py
+@Time    : 2023/06/20 10:32:18
+@Author  : HuJi
+@Contact : shootergod@forxmail.com
+@Version : 0.1
+@Desc    : None
+'''
+
 
 import random
 import plot_tools
